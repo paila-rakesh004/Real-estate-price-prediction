eafbe50ecd87e4108799eb9dabae99eaad3c15d9
paila-rakesh004 <pailarakesh004@gmail.com>
2025-07-21 19:24:51 +0530

    model.py

--- model.py
+++ model.py
@@ -2,12 +2,12 @@ import pandas as pd
 import joblib
 from sklearn.linear_model import LinearRegression
 
-# Sample data (Area in sqft, Bedrooms, Age in years, Price in ₹L)
+# Sample data (Area in sqft, Bedrooms, number of floors(stories), Price in ₹L)
 data = pd.read_csv('real_estate.csv')
-X = data[['area', 'bedrooms', 'stories']]  # Fixed bracket typo here
+X = data[['area', 'bedrooms', 'stories']]  
 y = data['price']
 model = LinearRegression()
 model.fit(X, y)
 
-joblib.dump(model, 'real_estate_model.pkl')  # Fixed file extension from 'pk1' to 'pkl'
+joblib.dump(model, 'real_estate_model.pkl')  
 print("Model saved as real_estate_model.pkl")
